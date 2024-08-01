86caa3dddcc0df3b8bd592152b747524f4ef6243
imranabdisalan7 <agmraan@gmail.com>
2024-08-01 10:48:20 +0300

    fixed a bug

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import requests
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,7 +15,7 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
-
+    CORS(app) 
     # Import models here to avoid circular imports
     with app.app_context():
         import models  # Ensure models are imported after db is initialized
@@ -22,6 +25,33 @@ def create_app():
     app.register_blueprint(course_bp, url_prefix='/courses')
     app.register_blueprint(enrollment_bp, url_prefix='/enrollments')
     app.register_blueprint(student_bp, url_prefix='/students')
+
+    # Add YouTube API route
+    @app.route('/youtube_videos', methods=['GET'])
+    def youtube_videos():
+        api_key = 'AIzaSyAuu1LOJKCFPEg1dXLAYgL5DrUOFgSMbP4'
+        search_query = 'learning python,javascript,Html'
+        max_results = 700
+        search_url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&q={search_query}&maxResults={max_results}&type=video&key={api_key}'
+        
+        response = requests.get(search_url)
+        if response.status_code == 200:
+            videos = response.json().get('items', [])
+            video_data = []
+            for video in videos:
+                video_id = video['id']['videoId']
+                video_title = video['snippet']['title']
+                video_description = video['snippet']['description']
+                video_url = f'https://www.youtube.com/watch?v={video_id}'
+                video_data.append({
+                    'id': video_id,
+                    'title': video_title,
+                    'description': video_description,
+                    'url': video_url
+                })
+            return jsonify(video_data)
+        else:
+            return jsonify({'error': 'Failed to fetch videos'}), response.status_code
 
     return app
 
