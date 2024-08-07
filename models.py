58e9ae2fb3d4d22362feeec7be7da2e14b3b175b
imranabdisalan7 <agmraan@gmail.com>
2024-08-07 19:36:38 +0300

    testing login

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
+import json
 import re
 
 metadata = MetaData()
@@ -33,3 +34,31 @@ class User(db.Model):
             "email": self.email,
             "role": self.role,
             "created_at": str(self.created_at), }
+    
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image = db.Column(db.String(255), nullable=True)
+    video = db.Column(db.String(255), nullable=True)
+    tech_stack = db.Column(db.String(255), nullable=True)
+    what_you_will_learn = db.Column(db.Text, nullable=True)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.truncate_description(self.description),
+            'image': self.image,
+            'video': self.video,
+            'techStack': self.tech_stack.split(',') if self.tech_stack else [],
+            'whatYouWillLearn': json.loads(self.what_you_will_learn) if self.what_you_will_learn else []
+        }
+    
+    def truncate_description(self, description, max_length=200):
+        """Truncate description to a maximum length and add ellipsis."""
+        if description and len(description) > max_length:
+            return description[:max_length] + '...'
+        return description
